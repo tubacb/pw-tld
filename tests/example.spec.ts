aca3f63966819d73013fb17a40f8cbeff815be6a
tuba bilgin <tubacobann@gmail.com>
2025-11-20 21:56:41 +0200

    faker library and env variable is used

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,22 @@
 import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker/locale/ar";
 
 test("test to sign in with randomly generated username and password", async ({ page }) => {
-  await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
-  await page.getByTestId("username-input").fill('test');
-  await page.getByTestId("password-input").fill('testtest');
-  await page.getByTestId("signIn-button").click();
-  await expect(
-    page.getByTestId("authorizationError-popup-close-button"),
-  ).toBeVisible();
+    await page.goto(process.env.APP_URL); //'https://fe-delivery.tallinn-learning.ee/signin'
+    await page.getByTestId("username-input").fill(faker.internet.username());
+    await page.getByTestId("password-input").fill(faker.internet.password());
+    await page.getByTestId("signIn-button").click();
+    await expect(
+        page.getByTestId("authorizationError-popup-close-button"),
+    ).toBeVisible();
 });
 
 test("password less than8", async ({ page }) => {
-  await page.goto("https://fe-delivery.tallinn-learning.ee/signin");
-  await page.getByTestId("username-input").fill('tes');
-      await page.getByTestId("password-input").fill('tes');
-  await expect(page.getByTestId("signIn-button")).toBeDisabled();
-  await expect(
-    page.getByText("the field must contain at least of characters: 8Sign in"),
-  ).toBeEnabled();
+    await page.goto(process.env.APP_URL);
+    await page.getByTestId("username-input").fill(faker.internet.username().slice(0, 3));
+    await page.getByTestId("password-input").fill(faker.internet.password({ length: 3 }));
+    await expect(page.getByTestId("signIn-button")).toBeDisabled();
+    await expect(
+        page.getByText("the field must contain at least of characters: 8Sign in"),
+    ).toBeEnabled();
 });
