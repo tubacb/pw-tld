ba49ed18ceec3c75381c29905855a5f12e17e2d8
tuba bilgin <tubacobann@gmail.com>
2025-11-24 15:05:02 +0200

    changed name of the test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,12 +11,12 @@ test("test to sign in with randomly generated username and password", async ({ p
     ).toBeVisible();
 });
 
-test("password less than8", async ({ page }) => {
+test("to test to show warning and disabled sign in button when password and username less than 8 characters", async ({ page }) => {
     await page.goto(process.env.APP_URL);
     await page.getByTestId("username-input").fill(faker.internet.username().slice(0, 3));
     await page.getByTestId("password-input").fill(faker.internet.password({ length: 3 }));
     await expect(page.getByTestId("signIn-button")).toBeDisabled();
     await expect(
         page.getByText("the field must contain at least of characters: 8Sign in"),
-    ).toBeEnabled();
+    ).toBeVisible();
 });
